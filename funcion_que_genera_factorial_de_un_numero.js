66040c0168907fe61a59d33da6270f8f44f9a6b9
manuel blanco <mandresblac@gmail.com>
2022-12-02 18:38:16 -0500

    commit 9

--- funcion_que_genera_factorial_de_un_numero.js
+++ funcion_que_genera_factorial_de_un_numero.js
@@ -1,4 +1,14 @@
 //Funcion para gerar el factorial de un número
+
+/* El factorial de un entero positivo n, se define como el producto de todos los números enteros positivos desde 1 hasta n, es decir
+, el factorial de 7 seria: 1*1= 1, 1*2= 2, 2*3= 6, 6*4 = 24, 24*5 = 120, 120*6 = 720, 720*7 = 5040  y si sucesivamente */
+
+/* La función factorial se representa con un signo de exclamación “!” detrás de un número. Esta exclamación quiere decir que hay
+   que multiplicar todos los números enteros positivos que hay entre ese número y el 1, por ejemplo:
+
+   7! = 1x2x3x4x5x6x7 = 5040
+*/
+
 const factorial = n => {
   //Condicion para validar si el numero insertado es menor o igual a 0
   if(n <= 0){
