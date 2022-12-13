cc8b3d0675b2205c393e6927f0a4ae35a31d2b7a
manuel blanco <mandresblac@gmail.com>
2022-12-12 19:56:44 -0500

    commit 12

---
+++ acceder_a_ultimo_caracter_de_cadena_caracteres.js
@@ -0,0 +1,10 @@
+let miCadena;
+miCadena = "JavaScript";
+
+//   Cadena:     J   a   v   a   S   c   r   i   p   t
+//   Indices:    0   1   2   3   4   5   6   7   8   9
+// Negativos:  -10  -9  -8  -7  -6  -5  -4  -3  -2  -1
+
+//Como acceder al ultimo caracter de una cadena de caracteres
+console.log(miCadena[miCadena.length - 10]);
+
