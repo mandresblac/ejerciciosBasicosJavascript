0e6ca96debb4b27397dace0534a0b68ff9e4c571
manuel blanco <mandresblac@gmail.com>
2022-12-12 20:06:39 -0500

    commit 13

--- acceder_a_ultimo_caracter_de_cadena_caracteres.js
+++ acceder_a_ultimo_caracter_de_cadena_caracteres.js
@@ -1,10 +1,14 @@
-let miCadena;
+//Declaramos dos variables
+let miCadena, n;
+
+//Inicialisamos las variables declaradas anteriormente
 miCadena = "JavaScript";
+n = 2;
 
 //   Cadena:     J   a   v   a   S   c   r   i   p   t
 //   Indices:    0   1   2   3   4   5   6   7   8   9
 // Negativos:  -10  -9  -8  -7  -6  -5  -4  -3  -2  -1
 
 //Como acceder al ultimo caracter de una cadena de caracteres
-console.log(miCadena[miCadena.length - 10]);
+console.log(miCadena[miCadena.length - n]);
 
