2568ac6c1ffbabd62fb7f162afecf0c8ef1502f2
manuel blanco <mandresblac@gmail.com>
2022-12-17 20:09:45 -0500

    commit 19

---
+++ array-arreglos/array_de_objetos_lista_compras.js
@@ -0,0 +1,46 @@
+//Array de objetos que contiene lista de compras
+let miListaDeCompras = [
+  {
+    producto:"cereal",
+    cantidad: 3
+  },
+  {
+    producto:"leche",
+    cantidad: 2
+  },
+  {
+    producto:"galletas",
+    cantidad: 4
+  },
+  {
+    producto:"pan",
+    cantidad: 5
+  },
+  {
+    producto:"refresco",
+    cantidad: 9
+  },
+  {
+    producto:"pollo",
+    cantidad: 7
+  }
+];
+
+//Cereal
+console.log(`Voy a comprar ${miListaDeCompras[0].cantidad} unidades de ${miListaDeCompras[0].producto}.`);
+
+//Leche
+console.log(`Voy a comprar ${miListaDeCompras[1].cantidad} unidades de ${miListaDeCompras[1].producto}.`);
+
+//galletas
+console.log(`Voy a comprar ${miListaDeCompras[2].cantidad} unidades de ${miListaDeCompras[2].producto}.`);
+
+//Pan
+console.log(`Voy a comprar ${miListaDeCompras[3].cantidad} unidades de ${miListaDeCompras[3].producto}.`);
+
+//Refresco
+console.log(`Voy a comprar ${miListaDeCompras[4].cantidad} unidades de ${miListaDeCompras[4].producto}.`);
+
+//Pollo
+console.log(`Voy a comprar ${miListaDeCompras[5].cantidad} unidades de ${miListaDeCompras[5].producto}.`);
+
