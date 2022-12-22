64169e75b7c1d5a4f7a9d4c1915916c4f35d823b
manuel blanco <mandresblac@gmail.com>
2022-12-22 13:17:34 -0500

    commit 29

---
+++ ciclo_while/ejercicio_ciclo_while.js
@@ -0,0 +1,13 @@
+let miArreglo = []; //Declaramos un arreglo vacio
+let i = 0; //Declaramos la varible i y la inicializamos en cero
+
+console.log(miArreglo); //Mostramos el contenido el arreglo antes del bucle while
+
+//Ciclo while
+while(i < 10){
+  miArreglo.push(i + 1);
+  i++
+};
+
+console.log(miArreglo) // Mostramos el contenido de arreglo despues del bucle while
+
