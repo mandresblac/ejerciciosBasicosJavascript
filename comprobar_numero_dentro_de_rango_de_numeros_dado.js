95ca80f93b8eaa75a62ae2b10b31d3150c497c8f
manuel blanco <mandresblac@gmail.com>
2023-03-06 12:54:48 -0500

    commit 39

--- comprobar_numero_dentro_de_rango_de_numeros_dado.js
+++ comprobar_numero_dentro_de_rango_de_numeros_dado.js
@@ -3,11 +3,12 @@ Mostrar cuantos números del 1 al 100 contienen un número 9.
 */
 
 for(let i = 1; i <= 100; i++){
-    let numString = i.toString();//.toString() convierte a string la variale "i" que es un tipo number
-    if(numString.includes("9")) {
-	        console.log(`${numString}`)
-	    };
+  let numString = i.toString();//El metodo .toString() convierte a string la variale "i" que es un tipo number
+  if(numString.includes("9")) {
+	console.log(`${numString}`)
+  };
 };
+
 
 /*
  En el codigo anterior primero convertimos la variable "i" en un string y luego con el metodo .includes() de los string, verificamos que el
@@ -16,12 +17,11 @@ for(let i = 1; i <= 100; i++){
 
 console.log("");
 
-
-// AHORA CON EL NÚMERO CERO
+// Otra forma es utilizando el constructor String()
 for(let i = 1; i <= 100; i++){
-    let numString = i.toString();//.toString() convierte a string la variale "i" que es un tipo number
-    if(numString.includes("0")) {
-	        console.log(`${numString}`)
-	    };
+  let numString = String(i);//Usamos el constructor String() para convertir la variable "i" que es un number, a un string
+  if(numString.includes("0")) {
+	console.log(`${numString}, Contiene un 0`)
+  };
 };
 
