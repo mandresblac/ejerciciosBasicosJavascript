8631a3caa6416c9b6ecb65d3f0521726acd0671b
manuel blanco <mandresblac@gmail.com>
2023-05-18 19:54:15 -0500

    commit 41

--- generar_numeros_aleatorios/funcionNumeroAleatorioEntre1y10.js
+++ generar_numeros_aleatorios/funcionNumeroAleatorioEntre1y10.js
@@ -2,12 +2,20 @@
 //Funcion que devuelve un número aleatorio entre 1 y 10
 
 function getRandomNumber() {
-    const random = Math.random(); //Número aleatorio entre 0 y 1 (1 no incluido)
-    const multiplied = random * 10; //multiplicamos por 10 el numero aleatorio
-    const rounded = Math.floor(multiplied); //Redondeamos número aleatorio
-    const result = rounded + 1; //sumamos 1 para llegar al 10
+  // recuperamos un número aleatorio entre 0 y 1
+  const random = Math.random() // por ejemplo: 0.6803487380457318
 
-    return result;
+  // lo multiplicamos por 10 para que esté entre 0 y 10
+  const multiplied = random * 10 // -> 6.803487380457318
+
+  // redondeamos hacia abajo para que esté entre 0 y 9
+  const rounded = Math.floor(multiplied) // -> 6
+
+  // le sumamos uno para que esté entre 1 y 10
+  const result = rounded + 1 // -> 7
+
+  // devolvemos el resultado
+  return result
 };
 
 console.log(getRandomNumber());
